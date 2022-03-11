634c30729eb1b10b64b01d28c475364493d6f2fe
Mbodze <mbodzemaggie@yahoo.com>
2022-03-12 01:36:21 +0300

    Correcting 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,6 +3,27 @@
 /**
  * main - main block
  * Description - Use 'putchar' to print the lowercase alphabets
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description - Use 'putchar' to print the lowercase alphabets
+ * Return: 0
+ */
+int main(void)
+{
+        char a = 'a';
+
+        while (a <= 'z')
+        {
+                putchar(a);
+                a++;
+        }
+        putchar('\n');
+
+        return (0);
+}
+
  * Return: 0
  */
 int main(void)
